82bcfe68555d72fa74eab52260ee6708301adc31
John Doe <mattia.tummolo@libero.it>
2021-01-15 17:00:24 +0100

    Completamento esercizio.

--- index.php
+++ index.php
@@ -19,30 +19,92 @@
 <!-- Definire la classe Magazzino e la classe Prodotto nel seguente modo:
 Magazzino: definire gli attributi per nome, location e prodotti contenuti (array); definire inoltre costruttore con solo nome e location obbligatori
 Prodotto: definire gli attributi per nome, peso e prezzo
-Generare alcune istanze di ogni classe, hai fini di test e per meglio comprendere i concetti -->
+Generare alcune istanze di ogni classe, ai fini di test e per meglio comprendere i concetti -->
 
         <h1>
         
         
             <?php
-                //entità
-                class Magazzino{
-                    public name;
-                    public location;
-                    public prodotti; //array
+                //ENTITA' MAGAZZINO
+                class Magazzino {
+                    public $name;
+                    public $location;
+                    public $prodotti; //array
 
-                    public function __construct($name, $location) {
+                    public function __construct($name, $location, $prodotti = "non disponibili") {
 
                         $this -> name = $name;
                         $this -> location = $location;
+                        $this -> prodotti = $prodotti;
 
                     }
 
                 }
 
-                $magazzino = new Magazzino("Mag-1", "New York");
-                var_dump($magazzino);
+                //istanza con name e location obbligatori
+                $magazzino1 = new Magazzino("Mag-1", "New York");
 
+                $magazzino2 = new Magazzino("Mag-2", "Berlino", 
+                            [
+                                "palloni", 
+                                "tovaglioli", 
+                                "birre", 
+                                "patatine", 
+                                "cipolle", 
+                                "pomodori"
+                            ]
+                        );
+                
+                $magazzino3 = new Magazzino("Mag-3", "Napoli", 
+                            [
+                                "Fuochi d'artificio-1",
+                                "Fuochi d'artificio-2",
+                                "Fuochi d'artificio-3",
+                                "Fuochi d'artificio-4",
+                            ]
+                        );
+
+                //output
+                echo "<h2>MAGAZZINI</h2>";
+                var_dump($magazzino1);
+                echo "<br>";
+                var_dump($magazzino2);
+                echo "<br>";
+                var_dump($magazzino3);
+                echo "<br>";
+                        
+                
+
+                //ENTITA' PRODOTTO
+                class Prodotto {
+                    public $name;
+                    public $peso;
+                    public $prezzo;
+                    public $sconto = 0;
+
+                    public function __construct($name, $peso, $prezzo) {
+                        $this -> name = $name;
+                        $this -> peso = $peso;
+                        $this -> prezzo = $prezzo;
+                    }
+
+                }
+
+                
+
+                //istanza
+                $prodotto1 = new Prodotto("pallone", 200, 12.5);
+                $prodotto2 = new Prodotto("Computer", 2,735.99);
+
+
+                //output
+                echo "<h2>PRODOTTI</h2>";
+
+                var_dump($prodotto1);
+                echo "<br>";
+                var_dump($prodotto2);
+                echo "<br>";
+                
 
 
             ?>
